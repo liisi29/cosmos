f891e197c3b58fac2b4537071b056a7eb4989ef5
liisi.raidaru <liisi.raidaru@turnit.com>
2022-08-21 17:40:38 +0300

    Devops - added logging

--- server/src/app.js
+++ server/src/app.js
@@ -3,14 +3,23 @@
 // we can use it for routes and for using other middleware
 const express = require("express");
 const planetsRouter = require("./routes/planets/planets.router");
+const launchesRouter = require("./routes/launches/launches.router");
 const cors = require("cors");
 const path = require("path");
+const morgan = require("morgan");
+const fs = require("fs");
 
 const app = express();
 // added cors middleware
 app.use(cors({
     origin: "http://localhost:3000",
-}))
+}));
+// create a write stream (in append mode)
+var accessLogStream = fs.createWriteStream(
+    path.join(__dirname, 'access.log'), { flags: 'a' }
+);
+// setup the logger
+app.use(morgan("combined", { stream: accessLogStream }));  // combined is default
 
 
 // CHAIN of middleware
